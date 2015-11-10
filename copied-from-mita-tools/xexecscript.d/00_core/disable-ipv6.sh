57ec1b9165bfb45a3f52cb7be84dc18301ed1c4c
Richard Potter <potter@axsh.net>
2015-11-10 22:11:04 +0900

    Changed technique to disable IP6 for CentOS 7

--- copied-from-mita-tools/xexecscript.d/00_core/disable-ipv6.sh
+++ copied-from-mita-tools/xexecscript.d/00_core/disable-ipv6.sh
@@ -4,6 +4,22 @@
 #  bash
 #
 
+# The technique for centos7: (and maybe centos6)
+# https://wiki.centos.org/FAQ/CentOS5
+# https://wiki.centos.org/FAQ/CentOS6
+
+cat >>/etc/sysctl.conf <<'EOF'
+#
+# from https://wiki.centos.org/FAQ/CentOS7:
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+EOF
+
+exit 0 # the above replaces the one below
+
+# The technique below was for centos5:
+# https://wiki.centos.org/FAQ/CentOS5
+
 cat <<EOS > $1/etc/modprobe.d/disable-ipv6.conf
 install ipv6 /bin/true
 EOS
