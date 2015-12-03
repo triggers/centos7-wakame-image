469624eb2355ba9700bf962ab71a8f8f10514e9e
Richard Potter <potter@axsh.net>
2015-12-03 23:12:30 +0900

    Added install wakame-init step to centos-7.1.1503-x86_64/build.sh

--- build2/build.sh
+++ build2/build.sh
@@ -19,25 +19,6 @@ export DATADIR="/proc/self"  # i.e. not set yet
 cd -P /proc/self
 
 ######################################################################
-## Functions
-######################################################################
-
-# This function is piped and run through ssh below
-patch-wakame-init()
-{
-    org="$(cat /etc/wakame-init)"
-    beforeNICequals="${org%nic=*}"
-    afterNICequals="${org##*nic=}"
-    # Now rewrite the line by inserting new code and commenting out the old code
-    {
-	echo -n "$beforeNICequals"
-	# the grep will output something like this: /sys/class/net/nestbr0/address:b6:21:21:a2:8f:bf
-	echo -n 'IFS=/ read xempty xsys xclass xnet nic therest <<<"$(grep "$mac" /sys/class/net/*/address)" ## '
-	echo  "$afterNICequals"
-    } >/etc/wakame-init
-}
-
-######################################################################
 ## Build Steps
 ######################################################################
 
@@ -49,19 +30,6 @@ patch-wakame-init()
 exit
 
 ## Public wakame build
-
-(
-    $starting_step "Install wakame-init to public image"
-    [ -f "$DATADIR/flag-wakame-init-installed" ]
-    $skip_rest_if_already_done
-    set -e
-    repoURL=https://raw.githubusercontent.com/axsh/wakame-vdc/develop/rpmbuild/yum_repositories/wakame-vdc-stable.repo
-    "$CODEDIR/bin/ssh-shortcut.sh" curl "$repoURL" -o /etc/yum.repos.d/wakame-vdc-stable.repo --fail
-    "$CODEDIR/bin/ssh-shortcut.sh" yum install -y net-tools
-    "$CODEDIR/bin/ssh-shortcut.sh" yum install -y wakame-init
-    "$CODEDIR/bin/ssh-shortcut.sh" <<<"$(declare -f patch-wakame-init; echo patch-wakame-init)"
-    touch "$DATADIR/flag-wakame-init-installed"
-) ; prev_cmd_failed "Error while installing wakame-init"
 
 (
     $starting_step "Shutdown VM for public image installation"
--- build2/centos-7.1.1503-x86_64/build.sh
+++ build2/centos-7.1.1503-x86_64/build.sh
@@ -15,6 +15,25 @@ export DATADIR="$CODEDIR/output"
 cd -P /proc/self
 
 ######################################################################
+## Functions
+######################################################################
+
+# This function is piped and run through ssh below
+patch-wakame-init()
+{
+    org="$(cat /etc/wakame-init)"
+    beforeNICequals="${org%nic=*}"
+    afterNICequals="${org##*nic=}"
+    # Now rewrite the line by inserting new code and commenting out the old code
+    {
+	echo -n "$beforeNICequals"
+	# the grep will output something like this: /sys/class/net/nestbr0/address:b6:21:21:a2:8f:bf
+	echo -n 'IFS=/ read xempty xsys xclass xnet nic therest <<<"$(grep "$mac" /sys/class/net/*/address)" ## '
+	echo  "$afterNICequals"
+    } >/etc/wakame-init
+}
+
+######################################################################
 ## Build Steps
 ######################################################################
 
@@ -60,3 +79,16 @@ cd -P /proc/self
     done
     [[ "$tryssh" = "it-worked" ]]
 ) ; prev_cmd_failed "Error while booting fresh minimal image"
+
+(
+    $starting_step "Install wakame-init to public image"
+    [ -f "$DATADIR/flag-wakame-init-installed" ]
+    $skip_rest_if_already_done
+    set -e
+    repoURL=https://raw.githubusercontent.com/axsh/wakame-vdc/develop/rpmbuild/yum_repositories/wakame-vdc-stable.repo
+    "$DATADIR/ssh-shortcut.sh" curl "$repoURL" -o /etc/yum.repos.d/wakame-vdc-stable.repo --fail
+    "$DATADIR/ssh-shortcut.sh" yum install -y net-tools
+    "$DATADIR/ssh-shortcut.sh" yum install -y wakame-init
+    "$DATADIR/ssh-shortcut.sh" <<<"$(declare -f patch-wakame-init; echo patch-wakame-init)"
+    touch "$DATADIR/flag-wakame-init-installed"
+) ; prev_cmd_failed "Error while installing wakame-init"
