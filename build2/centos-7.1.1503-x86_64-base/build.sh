d147d612495f5a52401949c9b99351c15b45a007
Richard Potter <potter@axsh.net>
2015-12-08 14:55:38 +0900

    Refactored centos-7.1.1503-x86_64-base into separate step scripts

--- build2/centos-7.1.1503-x86_64-base/build.sh
+++ build2/centos-7.1.1503-x86_64-base/build.sh
@@ -22,71 +22,10 @@ source "$CODEDIR/build.conf"
 
 (
     $starting_dependents "Build centos-7.1.1503-x86_64-base image"
-    (
-	$starting_checks "Create output directory for base image"
-	[  -d "$DATADIR" ]
-	$skip_rest_if_already_done
-	mkdir "$DATADIR"
-    ) ; prev_cmd_failed
 
-    (
-	$starting_checks "Download CentOS ISO install image"
-	[ -f "$DATADIR/$CENTOSISO" ] &&
-	    [[ "$(< "$DATADIR/$CENTOSISO.md5")" = *$ISOMD5* ]]
-	$skip_rest_if_already_done
-	set -e
-	if [ -f "$CODEDIR/$CENTOSISO" ]; then
-	    # to avoid the download while debugging
-	    cp -al "$CODEDIR/$CENTOSISO" "$DATADIR/$CENTOSISO"
-	else
-	    curl --fail "$CENTOSMIRROR/$CENTOSISO" -o "$DATADIR/$CENTOSISO"
-	fi
-	md5sum "$DATADIR/$CENTOSISO" >"$DATADIR/$CENTOSISO.md5"
-    ) ; prev_cmd_failed "Error while downloading ISO image"
-
-    (
-	$starting_checks "Generate ssh key pair and kickstart file"
-	[ -f "$DATADIR/ks-sshpair.cfg" ]
-	$skip_rest_if_already_done
-	set -e
-	[ -f "$DATADIR/tmp-sshkeypair" ] || ssh-keygen -f "$DATADIR/tmp-sshkeypair" -N ""
-	ks_text="$(cat "$CODEDIR/anaconda-ks.cfg")"
-	sshkey_text="$(cat "$DATADIR/tmp-sshkeypair.pub")"
-	cat >"$DATADIR/ks-sshpair.cfg" <<EOF
-$ks_text
-
-%post
-ls -l /root/  >/tmp.listing
-mkdir /root/.ssh
-chmod 700 /root/.ssh
-cat >/root/.ssh/authorized_keys <<EOS
-$sshkey_text
-EOS
-%end
-EOF
-	cp "$CODEDIR/bin/ssh-shortcut.sh" "$DATADIR"
-    ) ; prev_cmd_failed "Error while creating custom ks file with ssh key"
-
-    (
-	$starting_checks "Install minimal image with kickstart"
-	[ -f "$DATADIR/minimal-image.raw" ] || \
-	    [ -f "$DATADIR/minimal-image.raw.tar.gz" ]
-	$skip_rest_if_already_done
-	set -e
-	cd "$DATADIR"  # centos-kickstart-build.sh creates files in the current $(pwd)
-	time "$CODEDIR/bin/centos-kickstart-build.sh" \
-	     "$CENTOSISO" "ks-sshpair.cfg" "tmp.raw" 1024M
-	cp -al "tmp.raw" "minimal-image.raw"
-    ) ; prev_cmd_failed "Error while installing minimal image with kickstart"
-
-    (
-	$starting_checks "Tar minimal image"
-	[ -f "$DATADIR/minimal-image.raw.tar.gz" ]
-	$skip_rest_if_already_done
-	set -e
-	cd "$DATADIR/"
-	time tar czSvf minimal-image.raw.tar.gz minimal-image.raw
-    ) ; prev_cmd_failed "Error while tarring minimal image"
+    for i in "$CODEDIR/steps-to-do"/*.sh; do
+	"$i" ; prev_cmd_failed
+    done
 
     $starting_checks
     true # this step just groups the above steps
