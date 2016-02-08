b4e27c45c183b1341bbf1555f77bcb846224cacb
k-oyakata <k-sasaki@axsh.net>
2016-02-08 19:49:28 +0900

    fixing rc.local (permission, operation of the zabbix and the td-agent)

--- copied-from-mita-tools/xexecscript.d/81_mita-zabbix-agent-init/install_wakame-init.sh
+++ copied-from-mita-tools/xexecscript.d/81_mita-zabbix-agent-init/install_wakame-init.sh
@@ -8,11 +8,13 @@ set -e
 declare chroot_dir=$1
 
 cat <<-'EOS' >> ${chroot_dir}/etc/rc.d/rc.local
-	/etc/init.d/zabbix-agent stop
+	/usr/bin/systemctl stop zabbix-agent
 	Hostname=$(cat /metadata/meta-data/instance-id)
 	ListenIP=$(cat /metadata/meta-data/local-ipv4)
 	Server="$(echo $(cat /metadata/meta-data/x-monitoring/zabbix-servers) | sed "s/ /,/g;")"
 	sed "s,^Hostname=.*,Hostname=${Hostname},; s,^ListenIP=.*,ListenIP=${ListenIP},; s/^Server=.*/Server=${Server}/;" /etc/zabbix/zabbix_agentd.conf.tmpl > /etc/zabbix/zabbix_agentd.conf
-	/etc/init.d/zabbix-agent start
+	/usr/bin/systemctl start zabbix-agent
 	EOS
 cat ${chroot_dir}/etc/rc.d/rc.local
+chmod 755 ${chroot_dir}/etc/rc.d/rc.local
+ls -la ${chroot_dir}/etc/rc.d/rc.local
--- copied-from-mita-tools/xexecscript.d/82_mita-fluentd-init/install_wakame-init.sh
+++ copied-from-mita-tools/xexecscript.d/82_mita-fluentd-init/install_wakame-init.sh
@@ -9,6 +9,9 @@ declare chroot_dir=$1
 
 cat <<-'EOS' >> ${chroot_dir}/etc/rc.d/rc.local
         # need to start td-agent after running /etc/wakame-init
-	/etc/init.d/td-agent start
+	/etc/init.d/td-agent restart
 	EOS
 cat ${chroot_dir}/etc/rc.d/rc.local
+chmod 755 ${chroot_dir}/etc/rc.d/rc.local
+ls -la ${chroot_dir}/etc/rc.d/rc.local
+
